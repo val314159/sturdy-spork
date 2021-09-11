1b9a7bec4015438aa285f2f30996940ac7626216
@val314159 <jmward@gmail.com>
2021-09-11 20:06:08 +0000

    update

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 call: clean all
 all: uniswap_contracts
-out/IERC20.abi: interfaces/IERC20.sol
-	solc interfaces/*.sol -oout --abi
-uniswap_contracts: uniswap-v2-core/out uniswap-v3-core/out out/IERC20.abi
-	cp uniswap-v?-core/out/*.abi out
+#out/IERC20.abi: interfaces/IERC20.sol
+#	solc interfaces/*.sol -oout --abi
+uniswap_contracts: uniswap-v2-core/out uniswap-v3-core/out
+	mkdir -p out ; cp uniswap-v?-core/out/*.abi out
 uniswap-v2-core/out:
 	cd uniswap-v2-core ; solc-0.5.16 contracts/*.sol -oout --abi --bin
 uniswap-v3-core/out:
--- bnt.py
+++ bnt.py
@@ -2,19 +2,140 @@ import os, sys, json, time, web3
 from web3 import Web3, HTTPProvider
 from pprint import pprint
 
-url = "https://mainnet.infura.io/v3/206f5160ae22470faee089b2ed352c49"
-w3 = Web3(Web3.HTTPProvider(url))
-#w3 = Web3()
+#url = "https://mainnet.infura.io/v3/206f5160ae22470faee089b2ed352c49"
+#w3 = Web3(Web3.HTTPProvider(url))
+w3 = Web3()
 
+# need for this infura
+# doesn't seem to break anything on ganache-cli
 from web3.middleware import geth_poa_middleware
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+a0="0xee98420CfE9A94Fc6532E5fE89d92DD60718871F"
+a1="0xf7A4ca3d1Dc5817336CB9580822701F31Cc5A9eC"
+
+p0="0xb75425430db0b9db1b371a118fb626bbf8aeb0d713601aac3fdd5c05d28ed637"
+p1="0x4708782b211d897c53c532513b8b27711c10d83f344ece1b18400daa0af9fc3b"
 
 if w3.isConnected():
     print(">> connected!")
 else:
     raise Exception('not connected')
 
+print(w3.eth.get_balance(a0))
+print(w3.eth.get_balance(a1))
+
+#import web3
+#from web3 import Web3
+from uniswap import Uniswap
+from decimal import Decimal
+#from kista import *
+import kista
+
+#w3 = Web3()
+
+#print(dir(web3))
+#e = Web3.fromWei(3841357360894980500000001, 'ether')
+#print(e)
+
+if 0:
+    w = Web3.toWei(3.1, 'ether')
+    print(w, 'wei')
+
+    e = Web3.fromWei(w, 'ether')
+    print(e, 'eth')
+    pass
+
+#address_key = "YOUR ADDRESS"          # or None if you're not going to make transactions
+#private_key = "YOUR PRIVATE KEY"  # or None if you're not going to make transactions
+address = None
+private_key = None
+version = 1
+#version = 2                       # specify which version of Uniswap to use
+#version = 3                       # specify which version of Uniswap to use
+#provider = "WEB3 PROVIDER URL"    # can also be set through the environment variable `PROVIDER`
+#provider = "https://ropsten.infura.io/206f5160ae22470faee089b2ed352c49"
+#provider = "https://mainnet.infura.io/v3/206f5160ae22470faee089b2ed352c49"
+#uniswap = Uniswap(address=address, private_key=private_key, version=version, provider=provider)
+
+#provider = "https://mainnet.infura.io/v3/206f5160ae22470faee089b2ed352c49"
+uniswap = Uniswap(address=address, private_key=private_key, version=version)
+
+erc20 = dict()
+for k, v in dict(
+        weth='0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+).items():
+    print(k, v)
+    print(Web3.toChecksumAddress(v))
+    erc20[k] = Web3.toChecksumAddress(v)
+    pass
+
+print(erc20)
+
+a = erc20['weth']
+print("A", a)
+print("A", type(a))
+
+print(kista.load_abi('IERC20'))
+
+abi = kista.load_abi('IERC20')
+
+c0 = w3.eth.contract(address=a, abi=abi)
+c1 = kista.WrapContract(c0)
+print(c1.symbol())
+print(c1.totalSupply())
+#rint(c0.functions())
+#a2 = WrapAccount(a)
+
+#print("A", a2)
+
+#print("A", a2.balance)
+
+print("-----")
+
+x1 = uniswap.get_ex_eth_balance(erc20['weth'])
+
+print(x1)
+
+exit()
+
+for k,v in erc20:
+    print(k, v)
+
+#Web3.toChecksumAddress(lower_case_address).', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
 
 
+x1 = uniswap.get_ex_eth_balance(erc20['weth'])
 
+exit()
+
+# Some token addresses we'll be using later in this guide
+eth = "0x0000000000000000000000000000000000000000"
+bat = "0x0D8775F648430679A709E98d2b0Cb6250d2887EF"
+dai = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+xai = "0x2a1530C4C41db0B0b2bB646CB5Eb1A67b7158667"
+bat = xai
+
+w = uniswap.get_price_input(eth, xai, 10**18)
+#d = Web3.fromWei(w, 'ether')
+print(w, 'xai')
+
+# Get the balance of ETH in an exchange contract.
+x1 = uniswap.get_ex_eth_balance(bat)
+
+# Get the balance of a token in an exchange contract.
+x2 = uniswap.get_ex_token_balance(bat)
+
+# Get the exchange rate of token/ETH
+x3 = uniswap.get_exchange_rate(bat)
+
+print(x1,x2,x3)
+
+#Liquidity Methods (v1 only)¶
+
+# Add liquidity to the pool.
+#uniswap.add_liquidity(bat, 1*10**18)
+
+# Remove liquidity from the pool.
+#uniswap.remove_liquidity(bat, 1*10**18)
 
