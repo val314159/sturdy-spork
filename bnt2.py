b69b569dbcdee8eaebfc8afbd206922bf796f6d7
@val314159 <jmward@gmail.com>
2021-09-12 20:40:54 +0000

    update

--- bnt2.py
+++ bnt2.py
@@ -23,15 +23,35 @@ if w3.isConnected():
 else:
     raise Exception('not connected')
 
+from uniswap import Uniswap
+from decimal import Decimal
+#from kista import *
+import kista
+
+def LoadContract(address, name):
+    return kista.WrapContract(
+        w3.eth.contract(address=address, abi=kista.load_abi(name)))
+
+def LoadContract2(name, address):
+    return LoadContract(address, name)
+
+#c0 = LoadContract2( 'UniswapV3Factory',                   '0x1F98431c8aD98523631AE4a59f267346ea31F984')
+#c1 = LoadContract2( 'Multicall2',                         '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696')
+#c2 = LoadContract2( 'ProxyAdmin',                         '0xB753548F6E010e7e680BA186F9Ca1BdAB2E90cf2')
+#c3 = LoadContract2( 'TickLens',                           '0xbfd8137f7d1516D3ea5cA83523914859ec47F573')
+#c4 = LoadContract2( 'Quoter',                             '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6')
+#c5 = LoadContract2( 'SwapRouter',                         '0xE592427A0AEce92De3Edee1F18E0157C05861564')
+#c6 = LoadContract2( 'NFTDescriptor',                      '0x42B24A95702b9986e82d421cC3568932790A48Ec')
+#c7 = LoadContract2( 'NonfungibleTokenPositionDescriptor', '0x91ae842A5Ffd8d12023116943e72A606179294f3')
+#c8 = LoadContract2( 'TransparentUpgradeableProxy',        '0xEe6A57eC80ea46401049E92587E52f5Ec1c24785')
+#c9 = LoadContract2( 'NonfungiblePositionManager',         '0xC36442b4a4522E871399CD717aBDD847Ab11FE88')
+#ca = LoadContract2( 'V3Migrator',                         '0xA5644E29708357803b5A882D272c41cC0dF92B34')
+
 #print(w3.eth.get_balance(a0))
 #print(w3.eth.get_balance(a1))
 
 #import web3
 #from web3 import Web3
-from uniswap import Uniswap
-from decimal import Decimal
-#from kista import *
-import kista
 
 #w3 = Web3()
 
@@ -76,13 +96,13 @@ uniswap3 = Uniswap("","",version=3,web3=w3)
 erc20 = dict()
 e00='0x0000000000000000000000000000000000000000'
 for k, v in dict(
-        e00='0x0000000000000000000000000000000000000000',
-        eth='0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-        dai='0x6b175474e89094c44da98b954eedeac495271d0f',
-        btc='0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
-        usd='0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-        uni='0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
-        bat="0x0D8775F648430679A709E98d2b0Cb6250d2887EF",
+        e00 ='0x0000000000000000000000000000000000000000',
+        weth='0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+        dai ='0x6b175474e89094c44da98b954eedeac495271d0f',
+        wbtc='0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+        usdc='0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+        uni ='0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+        bat ="0x0D8775F648430679A709E98d2b0Cb6250d2887EF",
 ).items():
     #print(k, v)
     #print(Web3.toChecksumAddress(v))
@@ -95,19 +115,61 @@ for k, v in dict(
 #print("A", type(a))
 #print(kista.load_abi('IERC20'))
 
-def LoadContract(address, name):
-    return kista.WrapContract(
-        w3.eth.contract(address=address, abi=kista.load_abi(name)))
+#factory_address = "0x1F98431c8aD98523631AE4a59f267346ea31F984"
+factory = LoadContract2("UniswapV3Factory", "0x1F98431c8aD98523631AE4a59f267346ea31F984")
 
-factory_address = "0x1F98431c8aD98523631AE4a59f267346ea31F984"
-factory = LoadContract(factory_address, "UniswapV3Factory")
+fee = 10000
+fee =   500
+fee =  3000
 
-a1 = factory_address
-a2 = factory_address
+def get_pool(token0=None, token1=None, fee=3000, symbol0=None, symbol1=None):
+    if token0 is None: token0 = erc20[symbol0]
+    if token1 is None: token1 = erc20[symbol1]
+    return factory.getPool(token0, token1, fee)
 
-print("F", factory.owner())
-print("F", factory.feeAmountTickSpacing(11))
-print("F", factory.getPool(a1,a2,1))
+#a1 = erc20['weth']
+#a2 = erc20['dai']
+
+print("F1", factory.owner())
+print("F2", factory.feeAmountTickSpacing(11))
+
+#print("PL", factory.getPool(a1,a2,fee))
+#print("PL", get_pool(a1,a2,fee))
+
+pool_address = get_pool(symbol0="weth",symbol1="dai")
+
+print("PL", pool_address)
+
+pool = LoadContract(pool_address, "UniswapV3Pool")
+
+print(pool.token0())
+print(pool.token1())
+
+tok0 = LoadContract(pool.token0(), "IERC20")
+tok1 = LoadContract(pool.token1(), "IERC20")
+
+print("b0", tok0.balanceOf(pool_address))
+print("b1", tok1.balanceOf(pool_address))
+
+print(pool.factory())
+print(pool.fee())
+print(pool.token0())
+print(pool.token1())
+#print(a1)
+#print(a2)
+print("X")
+print(pool.tickSpacing())
+print(pool.maxLiquidityPerTick())
+print(pool.slot0())
+for n in range(1600000):
+    ob = pool.observations(n)
+    if not ob[-1]:
+        break
+    print(n,ob)
+    pass
+#print(n,pool.observations(n))
+#print(pool.balance0())
+#print(pool.balance1())
 
 exit()
 
@@ -125,7 +187,7 @@ for k, v in erc20.items():
         #print(2, w, 'weii')
         #w = uniswap2.get_price_input(v, e00, 10**18)
         #print(2, w, 'weii')
-        w = uniswap3.get_price_input(v, erc20['eth'], 10**18)
+        w = uniswap3.get_price_input(v, erc20['weth'], 10**18)
         print(3, w, 'weii', 'weth')
     except ContractLogicError:
         print("ERR")
@@ -136,7 +198,7 @@ for k, v in erc20.items():
         print("ERR")
         pass
     try:
-        w = uniswap3.get_price_input(v, erc20['usd'], 10**18)
+        w = uniswap3.get_price_input(v, erc20['usdc'], 10**18)
         print(3, w, 'weii', 'usd')
     except ContractLogicError:
         print("ERR")
